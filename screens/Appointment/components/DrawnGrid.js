bf05c9736c71716a366118a1b33fc9fc1fb6da8f
Hoang Le <hoang.le4@pcc.edu>
2019-11-06 14:18:36 -0800

    commit time adjustment

--- screens/Appointment/components/DrawnGrid.js
+++ screens/Appointment/components/DrawnGrid.js
@@ -3,40 +3,31 @@ import { View, StyleSheet, Text } from 'react-native';
 import HrLine from './HrLine';
 import TimeArr from '../constants/time';
 const DrawnGrid = ({ staff }) => {
-  return (
-    <View style={styles.drawnGridContainer}>
-      {staff.map((s, i) => {
-        return (
-          <View
-            style={(styles.drawnGridCol, [{ width: `${100 / staff.length}%` }])}
-            key={i}
-          >
-            {TimeArr.map((val, i) => (
-              <HrLine
-                key={i}
-                borderTop={i % 4 === 0 ? true : false}
-                time={val}
-              />
-            ))}
-          </View>
-        );
-      })}
-    </View>
-  );
+	return (
+		<View style={styles.drawnGridContainer}>
+			{staff.map((s, i) => {
+				return (
+					<View style={(styles.drawnGridCol, [ { width: `${100 / staff.length}%` } ])} key={i}>
+						{TimeArr.map((val, i) => <HrLine key={i} borderTop={i % 4 === 0 ? true : false} time={val} />)}
+					</View>
+				);
+			})}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  drawnGridContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    zIndex: 1,
-    flexWrap: 'wrap',
-    alignItems: 'flex-start'
-  },
-  drawnGridCol: {}
+	drawnGridContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		zIndex: 1,
+		flexWrap: 'wrap',
+		alignItems: 'flex-start'
+	},
+	drawnGridCol: {}
 });
 
 DrawnGrid.defaultProps = {
-  staff: [{ name: 'Hoang Le' }]
+	staff: [ { name: 'Hoang Le' } ]
 };
 export default DrawnGrid;
--- screens/Appointment/components/TimeCol.js
+++ screens/Appointment/components/TimeCol.js
@@ -3,34 +3,34 @@ import { View, Text, StyleSheet } from 'react-native';
 import TimeArr from '../constants/time';
 
 const TimeCol = ({ hour_size }) => (
-  <View style={{ paddingBottom: hour_size / 2 }}>
-    {TimeArr.map((val, i) => (
-      <View
-        style={[
-          styles.box,
-          {
-            height: hour_size,
-            borderRightColor: '#bababa',
-            borderRightWidth: 1
-          }
-        ]}
-        key={val}
-      >
-        <Text style={styles.textStyle}>{i % 4 === 0 ? val : ''}</Text>
-      </View>
-    ))}
-  </View>
+	<View style={{ paddingBottom: hour_size / 2 }}>
+		{TimeArr.map((val, i) => (
+			<View
+				style={[
+					styles.box,
+					{
+						height: hour_size + 0.25,
+						borderRightColor: '#bababa',
+						borderRightWidth: 1
+					}
+				]}
+				key={val}
+			>
+				<Text style={styles.textStyle}>{i % 4 === 0 ? val : ''}</Text>
+			</View>
+		))}
+	</View>
 );
 
 let styles = StyleSheet.create({
-  box: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textStyle: {
-    textAlign: 'center',
-    color: '#BABABA'
-  }
+	box: {
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	textStyle: {
+		textAlign: 'center',
+		color: '#BABABA'
+	}
 });
 
 export default TimeCol;
