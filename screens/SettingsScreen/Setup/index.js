2d3c88b70ac5c38d87f17f88995bda404bb34f14
Hoang Le <hoang.le4@pcc.edu>
2019-11-08 20:17:17 -0800

    commit subroute

--- screens/SettingsScreen/Setup/index.js
+++ screens/SettingsScreen/Setup/index.js
@@ -2,57 +2,67 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ListItem } from 'react-native-elements';
 import { ScrollView, StyleSheet, View, Text } from 'react-native';
-const Setup = props => {
-  const list = [
-    {
-      title: 'Account Settings',
-      subtitle: 'Manage settings your business name, address, etc...'
-    },
-    {
-      title: 'Locations',
-      subtitle: 'Add and remove locations'
-    },
-    {
-      title: 'Closed Dates',
-      subtitle: 'Shedule upcoming business closures or public holidays'
-    },
-    {
-      title: 'Online Booking',
-      subtitle: 'Edit your online business profile'
-    },
-    {
-      title: 'Marketing',
-      subtitle: 'Boost your sales with our amazing marketing team'
-    },
-    {
-      title: 'Client Messages',
-      subtitle: 'Manage client messages and review messages'
-    }
-  ];
-  return (
-    <ScrollView>
-      {list.map((item, i) => (
-        <ListItem
-          subtitle={
-            <View style={styles.subtitleView}>
-              <Text style={styles.ratingText}>{item.subtitle}</Text>
-            </View>
-          }
-          key={i}
-          title={item.title}
-          leftIcon={{
-            name: item.icon,
-            type: 'FontAwesome',
-            iconStyle: { fontSize: 40 }
-          }}
-          bottomDivider
-          chevron
-          onPress={e => props.navigation.navigate('Setup')}
-        />
-      ))}
-    </ScrollView>
-  );
+const Setup = (props) => {
+	const list = [
+		{
+			title: 'Account Settings',
+			subtitle: 'Manage settings your business name, address, etc...'
+		},
+		{
+			title: 'Locations',
+			subtitle: 'Add and remove locations'
+		},
+		{
+			title: 'Closed Dates',
+			subtitle: 'Shedule upcoming business closures or public holidays'
+		},
+		{
+			title: 'Online Booking',
+			subtitle: 'Edit your online business profile'
+		},
+		{
+			title: 'Marketing',
+			subtitle: 'Boost your sales with our amazing marketing team'
+		},
+		{
+			title: 'Client Messages',
+			subtitle: 'Manage client messages and review messages'
+		}
+	];
+	return (
+		<ScrollView>
+			{list.map((item, i) => (
+				<ListItem
+					subtitle={
+						<View style={styles.subtitleView}>
+							<Text style={styles.ratingText}>{item.subtitle}</Text>
+						</View>
+					}
+					key={i}
+					title={item.title}
+					leftIcon={{
+						name: item.icon,
+						type: 'FontAwesome',
+						iconStyle: { fontSize: 40 }
+					}}
+					bottomDivider
+					chevron
+					onPress={(e) => props.navigation.navigate('Setup')}
+				/>
+			))}
+		</ScrollView>
+	);
 };
 
 Setup.propTypes = {};
+const styles = StyleSheet.create({
+	subtitleView: {
+		flexDirection: 'row',
+		paddingTop: 5
+	},
+
+	ratingText: {
+		color: 'grey'
+	}
+});
 export default Setup;
---
+++ screens/SettingsScreen/Staff/StaffMembers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ListItem } from 'react-native-elements';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
+const StaffMember = (props) => {
+	return (
+		<ScrollView>
+			<Text>SubView</Text>
+		</ScrollView>
+	);
+};
+
+StaffMember.propTypes = {};
+
+export default StaffMember;
